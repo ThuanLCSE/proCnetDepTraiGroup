cc6da888adaa5f72f7e4e5de44c6d46d56410ac7
Nhoc Ring <thuanlcse61425@fpt.edu.vn>
2015-03-25 19:47:28 +0700

    Len khung

---
+++ projectDepTraiClub/projectDepTraiClub/objectInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace projectDepTraiClub
+{
+    interface objectInterface
+    {
+        public Boolean insert();
+        public Boolean update();
+        public Boolean delete();
+        public SqlDataReader select();
+    }
+}
