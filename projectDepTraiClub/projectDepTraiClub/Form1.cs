e99042c7c924a20e96081270e510a00c3e0b395a
Nhoc Ring <thuanlcse61425@fpt.edu.vn>
2015-03-26 01:19:00 +0700

    form hiep

--- projectDepTraiClub/projectDepTraiClub/Form1.cs
+++ projectDepTraiClub/projectDepTraiClub/Form1.cs
@@ -101,12 +101,105 @@ namespace Project
         private bool validateProduct()
         {
             bool error = true;
+            if (txtProductName.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtProductName, "Enter product name!!!");
+                error = false;
+            }
+            if (txtProSupplierID.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtProSupplierID, "Enter Supplier ID!!!");
+                error = false;
+            }
+            if (txtProCategoryID.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtProCategoryID, "Enter Category ID!!!");
+                error = false;
+            }
+            if (txtProUnitPrice.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtProUnitPrice, "Enter Unit Price!!!");
+                error = false;
+            }
+            if (radYes.Checked == false && radNo.Checked == false)
+            {
+                errorProvider1.SetError(groupBox1, "Please select status!");
+                error = false;
+            }
             return error;
         }
 
         private bool validateEmp()
         {
             bool error = true;
+            if (txtEmpLastName.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpLastName, "Enter Employee's Last Name!!!");
+                error = false;
+            }
+            if (txtEmpFirstName.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpFirstName, "Enter Employee's First Name!!!");
+                error = false;
+            }
+            if (txtEmpTitle.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpTitle, "Enter Ship Name!!!");
+                error = false;
+            }
+            if (txtEmpTitleOfCourtesy.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtOrderShipAddress, "Enter Title of Courtesy!!!");
+                error = false;
+            }
+
+            DateTime currDate = DateTime.Now;
+            int currYear = currDate.Year;
+            DateTime DateofBirth = dtpEmpBirthdate.Value;
+            int birthYear = DateofBirth.Year;
+            DateTime HireDate = dtpEmpHireDate.Value;
+            int hireYear = HireDate.Year;
+            if (currYear - birthYear < 18)
+            {
+                errorProvider1.SetError(dtpEmpBirthdate, "Age must be greater than or equal to 18");
+                error = false;
+            }
+            if (currYear - birthYear < 0)
+            {
+                errorProvider1.SetError(dtpEmpBirthdate, "Hire date must before today!!!");
+                error = false;
+            }
+
+            if (txtEmpAddress.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpAddress, "Enter Employee Address!!!");
+                error = false;
+            }
+            if (txtEmpCity.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpCity, "Enter Employee City!!!");
+                error = false;
+            }
+            if (txtEmpRegion.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpRegion, "Enter Employee's Region!!!");
+                error = false;
+            }
+            if (txtEmpPosCode.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpPosCode, "Enter Postal Code!!!");
+                error = false;
+            }
+            if (txtEmpCountry.Text.Length == 0)
+            {
+                errorProvider1.SetError(txtEmpCountry, "Enter Employee's Country!!!");
+                error = false;
+            }
+            if (mtbEmpPhone.MaskCompleted == false)
+            {
+                errorProvider1.SetError(mtbEmpPhone, "Please enter require digit!!!");
+                error = false;
+            }
             return error;
         }
 
@@ -216,6 +309,17 @@ namespace Project
         }
 
         private void txtEmpPosCode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9) && e.KeyCode != Keys.Back)
+            {
+                e.SuppressKeyPress = true;
+            }
+            else
+                if (e.Shift == true)
+                    e.SuppressKeyPress = true;
+        }
+
+        private void txtMgrid_KeyDown(object sender, KeyEventArgs e)
         {
             if ((e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9) && e.KeyCode != Keys.Back)
             {
