7586861f5192564b5590d851df62a3d789811afd
Nhoc Ring <thuanlcse61425@fpt.edu.vn>
2015-03-26 16:22:11 +0700

    update DTO

---
+++ projectDepTraiClub/projectDepTraiClub/DTO/DTOabs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Project
+{
+    
+    abstract class DTOabs:objectInterface
+    {
+        public SqlCommand cmd = new SqlCommand();
+
+        public DTOabs()
+        {
+            cmd.Connection = (new DAO.getCon()).ConDB();
+        }
+        #region objectInterface Members
+
+        public bool insert(List<string> str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(int id, List<string> str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Data.SqlClient.SqlDataReader select()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ projectDepTraiClub/projectDepTraiClub/DTO/objectInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Project
+{
+    interface objectInterface
+    {
+         Boolean insert(List<String> str);
+         Boolean update(int id, List<String> str);
+         Boolean delete(int id);
+         SqlDataReader select();
+    }
+}
